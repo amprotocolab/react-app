f26897d130aefa5ff8a92849935c1e45a596fd6d
hamza-smh <63658773+hamza-smh@users.noreply.github.com>
2023-01-11 12:04:43 +0500

    Third Commit -Monster React App
    Task Completed -Class component

---
+++ component/card-component/card.jsx
@@ -0,0 +1,22 @@
+import { Component } from "react";
+import "./card.styles.css";
+
+class Card extends Component{
+
+    render(){
+            const { name, email , id} = this.props.monster;
+
+        return(  
+            <div className="card-container" key={id}>
+                        <img
+                            alt={`monster ${name}`}
+                            src={`https://www.robohash.org/${id}?set=set3&size=180x180`}
+
+                        />
+                        <h2>{name}</h2>
+                        <p>{email}</p>
+                    </div>
+        );
+    }
+}
+export default Card;
